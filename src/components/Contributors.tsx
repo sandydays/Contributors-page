36ceea2c80dfbbcfe11866f4ce78a32f78f41bf9
Sandhya <sandhyar725@gmail.com>
2019-06-15 14:51:02 +0800

    Modified code to fetch data of contributors to all repos of the source academy rather than just the frontend

--- src/components/Contributors.tsx
+++ src/components/Contributors.tsx
@@ -9,7 +9,7 @@ interface ContributorsState {
 }
 
 interface ContributorsArrayState {
-    contributors: ContributorsState[] 
+    contributors: ContributorsState[][]
 }
 
 class Contributors extends Component<{}, ContributorsArrayState> {
@@ -23,39 +23,66 @@ class Contributors extends Component<{}, ContributorsArrayState> {
     }
 
     componentDidMount() {
-       fetch('https://api.github.com/repos/source-academy/cadet-frontend/contributors')
-       .then(results => {
-           return results.json();
-       })
-       .then(contributors => {
-           let contributorList = contributors.map((contributor: any)=> {
-                return ({
-                    key: contributor.id,
-                    photo: contributor.avatar_url,
-                    githubPage: contributor.html_url,
-                    githubName: contributor.login,
-                    commits: contributor.contributions
-                })
-           })
-           this.setState({
-               contributors: contributorList
-           })
-       })
 
-    }
-    
-    
-    render() {
-        const { contributors } = this.state;
-        let contributorList = contributors.length ? (
-            contributors.map(contributor => {
-                return (
-                    <div key={contributor.key}>
-                        <img src={contributor.photo} alt="Image" height="200" width="200"/>
-                        <p><a href={contributor.githubPage}>{contributor.githubName}</a></p>
-                        <p>Number of commits: {contributor.commits}</p>
-                    </div>
+        fetch('https://api.github.com/orgs/source-academy/repos')
+        .then(results => {
+            return results.json();
+        })
+        .then(repos => {
+            const contributorLinks = repos.map((repo: any) => {
+                return repo.contributors_url;
+            })
+            return contributorLinks;
+        })
+        .then(endpoints => {
+            Promise.all(
+                endpoints.map((endpoint: string) => {
+                    return fetch(endpoint);
+                })
+            )
+            .then(responses => {
+                return Promise.all( 
+                    responses.map((res: any) => {
+                        return res.json();
+                    }) 
                 )
+            })
+            .then(contributorsByRepo => {
+                return Promise.all(
+                    contributorsByRepo.map((contributors: any) => {
+                        const contributorList = contributors.map((contributor: any) => {
+                            return ({
+                                key: contributor.id,
+                                photo: contributor.avatar_url,
+                                githubPage: contributor.html_url,
+                                githubName: contributor.login,
+                                commits: contributor.contributions
+                            });
+                        });
+                        this.setState({
+                            contributors: [...this.state.contributors, contributorList]
+                        })
+                    })
+                )
+            });
+            
+        });
+    }
+      
+    render() {
+        console.log(this.state)
+        const { contributors: repos } = this.state;
+        let contributorList = repos.length ? (
+            repos.map((repo: any) => {
+                return repo.map((contributor: ContributorsState) => {
+                    return (
+                        <div key={contributor.key}>
+                            <img src={contributor.photo} alt="Image" height="200" width="200"/>
+                            <p><a href={contributor.githubPage}>{contributor.githubName}</a></p>
+                            <p>Number of commits: {contributor.commits}</p>
+                        </div>
+                    );
+                });        
             })
         ) : (
             <h2>No contributors to the Source Academy yet!</h2>
